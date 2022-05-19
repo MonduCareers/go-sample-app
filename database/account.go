d0d82b5c897a084db2d7d49ddeb0980c69eed03c
arvryna <arv.pyrna@gmail.com>
2022-05-19 12:49:02 +0400

    Implement update balance feature

--- database/account.go
+++ database/account.go
@@ -14,14 +14,24 @@ func CreateAccount(db *sql.DB) (uuid.UUID, error) {
 }
 
 func GetAccountBalance(db *sql.DB, accountId uuid.UUID) (int, error) {
+	return getBalance(db, accountId)
+}
+
+func DepositAmount(db *sql.DB, accountId uuid.UUID, amount int) error {
+	existingBalance, err := getBalance(db, accountId)
+	if err != nil {
+		return err
+	}
+	newBalance := existingBalance + amount
+	query := "UPDATE accounts SET balance = $1 WHERE id = $2"
+	_, err = db.Exec(query, newBalance, accountId)
+	return err
+}
+
+func getBalance(db *sql.DB, accountId uuid.UUID) (int, error) {
 	query := "SELECT balance FROM accounts WHERE id = $1"
 	row := db.QueryRow(query, accountId)
 	var balance int
 	err := row.Scan(&balance)
 	return balance, err
-}
-
-func DepositAmount(db *sql.DB, accountId uuid.UUID, amount int) error {
-	// todo
-	return nil
 }
